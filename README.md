3b70be1a739ac7f7567ce0cf98091047cc1db5ff
unknown <james.yang516@outlook.com>
2023-02-21 20:02:13 +0800

    update

--- README.md
+++ README.md
@@ -8,3 +8,9 @@ C online build: https://godbolt.org/
 C course on Geektime: https://github.com/Becavalier/geektime-c
 
 C in geeks https://www.geeksforgeeks.org/c-programming-language
+
+## C Standard Library
+
+C Standard Library Reference: https://cplusplus.com/reference/clibrary/
+
+![](./images/c-standard-lib.png)
---
+++ str.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+#define STRLEN 14
+
+
+int main(void) {
+  char strA[STRLEN] = "Hello,";
+  char strB[] = " world!";
+  strncat(strA, strB, STRLEN - strlen(strA) - 1);
+  printf("%s\n", strA); 
+
+  long double num = -10.1234;
+  printf("%.2f\n", fabs(num));
+}
