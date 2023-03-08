a908405589287f390ed4a6994a6ee9f3af806a0e
James.Y.Yang (tech.cncd02.Newegg) 42035 <james.yang516@outlook.com>
2023-03-08 08:47:30 +0800

    update

--- io.c
+++ io.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 
-int main(void) {
-  printf("Enter some characters:\n");
-  FILE* fp = fopen("./temp.txt", "w+");
-  if (fp) {
-    char ch;
-    while (scanf("%c", &ch)) {
-      if (ch == 'z') break;
-      putc(ch, fp);
+int main(void)
+{
+    printf("Enter some characters:\n");
+    FILE *fp = fopen("./temp.txt", "w+");
+    if (fp)
+    {
+        char ch;
+        while (scanf("%c", &ch))
+        {
+            if (ch == 'z')
+                break;
+            putc(ch, fp);
+        }
     }
-  } else {
-    perror("File open failed.");
-  }
-  fclose(fp);
-  return 0;
+    else
+    {
+        perror("File open failed.");
+    }
+    fclose(fp);
+    return 0;
 }
--- str.c
+++ str.c
@@ -3,13 +3,13 @@
 #include <math.h>
 #define STRLEN 14
 
+int main(void)
+{
+    char strA[STRLEN] = "Hello,";
+    char strB[] = " world!";
+    strncat(strA, strB, STRLEN - strlen(strA) - 1);
+    printf("%s\n", strA);
 
-int main(void) {
-  char strA[STRLEN] = "Hello,";
-  char strB[] = " world!";
-  strncat(strA, strB, STRLEN - strlen(strA) - 1);
-  printf("%s\n", strA); 
-
-  long double num = -10.1234;
-  printf("%.2f\n", fabs(num));
+    long double num = -10.1234;
+    printf("%.2f\n", fabs(num));
 }
